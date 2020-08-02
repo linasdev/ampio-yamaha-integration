24e3412f67c5d884d125e5cc23332f680195611f
Linas Nikiperavičius <linas@linasdev.com>
2020-08-02 12:07:12 +0300

    Remove build mechanism

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python -m nuitka --follow-imports --standalone --show-progress --show-scons --remove-output ./entry.py
--- entry.py
+++
@@ -1,1 +0,0 @@
-from ampio_yamaha_integration.__main__ import main
